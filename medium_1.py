001ce7564ed8160b686c7ea999b0b6125df6b25b
Sharu Gowda <Umesh>
2022-06-30 12:07:49 -0500

    Find least num of unique ints removing k elements

--- medium_1.py
+++ medium_1.py
@@ -4,6 +4,18 @@ from typing import List
 
 
 class Heap_Solution:
+    """
+    Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+    Input: arr = [5,5,4], k = 1
+    Output: 1
+    Explanation: Remove the single 4, only 5 is left.
+
+    Input: arr = [4,3,1,1,3,3,2], k = 3
+    Output: 2
+    Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+    
+    """
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         if len(arr) <= k:
             return 0
